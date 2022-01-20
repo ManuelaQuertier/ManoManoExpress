774aaa538a8e162f77ce680fd1fd43b1aa36e8b6
Manuela Quertier <manuelaquertier@gmail.com>
2022-01-20 12:08:08 +0100

    header

---
+++ express.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>ManoManoExpress</title>
+</head>
+
+<body>
+    <div class="header">
+        <nav class="navbar navbar-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <img class="logo" src="assets/logoManomano.png" alt="logo">
+                <div class="localisation d-flex align-items-center">
+                    <img class="logoLoca" src="assets/localisation.png" alt="localisation">
+                    <p class="mb-0">ma position</p>
+                </div>
+                <div class="user">
+                    <img src="assets/person-circle.svg" alt="user">
+                    <img src="assets/caddie.png" alt="caddie" width="15px" height="15px">
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="drone"></div>
+    <h1>Hello World</h1>
+</body>
+
+</html>
