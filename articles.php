bba7124a8fd6e835ba4bd5aca2fca7958f75449b
Anthony69600 <a.verchere69@gmail.com>
2022-01-20 14:49:09 +0100

    articles

---
+++ articles.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>ManoManoExpress</title>
+</head>
+
+<body>
+    <div class="header">
+        <nav class="navbar navbar-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <img class="logoExpress" src="assets/images/logoManomano.png" alt="logo">
+                <div class="localisation d-flex align-items-center">
+                    <img class="logoLoca" src="assets/images/localisation.png" alt="localisation">
+                    <p class="mb-0">ma position</p>
+                </div>
+                <div class="user">
+                    <img src="assets/images/person-circle.svg" alt="user">
+                    <img src="assets/images/caddie.png" alt="caddie" width="15px" height="15px">
+                </div>
+            </div>
+        </nav>
+    </div>
+    <section class="articles">
+        <form action="" method="get">
+            <div id="search_articles">
+                <label for="search"></label>
+                <input type="text" name="search" id="search" placeholder="Effectuer une recherche sur ManoMano Express">
+            </div>
+        </form>
+
+        <h2>Plomberie chauffage</h2>
+
+        <h4>Articles éligibles pour la livraison du drône</h4>
+
+        <div>
+            <p>Corps thermostatique equerre 15/21 mâle / femelle laiton nickelé DANFOSS</p>
+            <p>Ref : 68383602</p>
+            <div class="article">
+                <div>
+                    <img src="assets/images/danfoss-équerre.jpeg">
+                </div>
+                <div>
+                    <input class="favorite styled" type="button" value="achat immédiat">
+                    <input class="favorite styled" type="button" value="ajouter au panier">
+                </div>
+            </div>    
+            <p>Prix : 12,59 euros / HT</p>        
+        </div>
+        <div>
+            <p>Corps thermostatique equerre 15/21 mâle / femelle laiton chromé DANFOSS</p>
+            <p>Ref : 68383595</p>
+            <div class="article">
+                <div>
+                    <img src="assets/images/danfoss-équerre2.jpeg">
+                </div>
+                <div>
+                    <input class="favorite styled" type="button" value="achat immédiat">
+                    <input class="favorite styled" type="button" value="ajouter au panier">
+                </div>
+            </div>    
+            <p>Prix : 21,69 euros / HT</p>        
+        </div>
+    </section>
+    
+</body>
+
+</html>
