7954446924e2f35149f08f8f15fda5ad4753cb6c
Manuela Quertier <manuelaquertier@gmail.com>
2022-01-20 16:52:54 +0100

    bootstraping

--- index.php
+++ index.php
@@ -11,40 +11,35 @@
 </head>
 
 <body>
+
     <div class="header">
         <nav class="navbar navbar-light">
             <div class="container-fluid">
-                <div>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div>
-                    <a href="/index.php"><img class="logoExpress" src="assets/images/logoManomano.png" alt="logo"></a>
-                </div>
-                <div class="localisation d-flex">
-                    <input class="search form-control" type="text" placeholder="Search" aria-label="Search">
-                </div>
-                <div class="urgent col">
-                    <a href="/express.php"><button class="btn btn-danger" type="button">Livraison Urgente</button></a>
+                <div class="row d-flex">
+                    <img class="logoExpress" src="assets/images/logoManomano.png" alt="logo">
                 </div>
                 <div class="user">
                     <img src="assets/images/person-circle.svg" alt="user">
                     <img src="assets/images/caddie.png" alt="caddie" width="15px" height="15px">
                 </div>
-            </div>
-            <div class="menu row">
-                <div class="menu_navigation">
-                    <Menu></Menu>
+                <div class="row localisation">
+                    <div class="col-6 p-0">
+                        <input class="search form-control" type="text" placeholder="Search" aria-label="Search">
+                    </div>
+                    <div class="urgent col-6 p-0 text-end">
+                        <a href="/express.php"><button class="rouge btn btn-danger" type="button">Livraison Urgente</button></a>
+                    </div>
                 </div>
-                <div class="menu_horizontal">
-                    <img class="" src="/assets/images/Capture2.PNG">
+                <div class="menu row">
+                    <div class="menu_horizontal">
+                        <img class="" src="/assets/images/Capture2.PNG">
+                    </div>
                 </div>
             </div>
         </nav>
     </div>
     <main class="main">
-        <h1>Hello Every Body !!</h1>
+        <h2>Hello EveryBody !!</h2>
     </main>
 </body>
 
