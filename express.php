51ab10e71481c7eb00b6e09cd5374a895de94bea
Manuela Quertier <manuelaquertier@gmail.com>
2022-01-20 16:17:04 +0100

    first page done

--- express.php
+++ express.php
@@ -13,7 +13,7 @@
 </head>
 
 <body>
-    <div class="header">
+    <div class="header bg-white">
         <nav class="navbar navbar-light">
             <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -31,8 +31,53 @@
             </div>
         </nav>
     </div>
-    <div class="drone"></div>
-    <h1>Hello World</h1>
+    <div class="drone container-fluid d-flex justify-content-between">
+        <p class="text-end align-self-center">Livraison express<br> de petits outillages<br> et matériaux par<br> drône.</p>
+        <h1 class="align-self-end pb-3">ManoMano Express</h1>
+    </div>
+    <div class="searchNav container d-flex flex-column justify-content-around">
+        <div class="container d-flex flex-column justify-content-between align-items-center">
+            <button type="button" class="urgence btn btn-light">
+                <h2 class="mb-0">Une Urgence?</h2>
+                <p class="mb-0">Contactez-nous par téléphone 📞</p>
+            </button>
+            <input type="text" class="research form-control mt-3" placeholder="🔍 Faire une recherche sur ManoMano Express">
+        </div>
+        <div class="buttonS d-flex justify-content-between">
+            <button type="button" class="btn btn-secondary">Outillages</button>
+            <button type="button" class="btn btn-secondary">Plomberie chauffage</button>
+            <button type="button" class="btn btn-secondary">Electricité</button>
+        </div>
+    </div>
+    <div class="formAdress container bg-white mt-4">
+        <h5 class="mb-0 text-center">Saisissez votre adresse de livraison</h5>
+        <p class="text-center mb-5"> ou activez la localisation</p>
+        <div class="row mb-4">
+            <div class="col-12">
+                <input class="form-control form-control-sm" type="text" placeholder="Entreprise ou nom du destinataire">
+            </div>
+        </div>
+        <div class="row mb-4">
+            <div class="col-2">
+                <input class="form-control form-control-sm" type="text" placeholder="n°">
+            </div>
+            <div class="col-10">
+                <input class="form-control form-control-sm" type="text" placeholder="nom de rue">
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col-4">
+                <input class="form-control form-control-sm" type="text" placeholder="code postal">
+            </div>
+            <div class="col-8">
+                <input class="form-control form-control-sm" type="text" placeholder="ville">
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+        <button type="button" class="btn btn-dark commander">Commander</button>
+        </div>
+
+    </div>
 </body>
 
 </html>
